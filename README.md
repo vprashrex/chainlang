8b548ea641c4011454be973e1b1118a6c3253f45
Prashant <vprashant5050@gmail.com>
2024-08-26 12:39:53 +0530

    code-update

---
+++ README.md
@@ -0,0 +1,2 @@
+# Author: Prashant Vasudevan (vprashant5050@gmail.com)
+## CHANGES: LANGCHAIN AGENT BUG FIX
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chainlang",
+    version="0.1",
+    author="Prashant",
+    author_email="vprashant5050@gmail.com",
+    description="Modified version of langchain that fix the bug of langchain agent tool",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/vprashrex/chainlang.git",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
